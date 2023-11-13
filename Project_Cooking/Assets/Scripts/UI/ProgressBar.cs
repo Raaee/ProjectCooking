d12021e375d67669133a0d43bc8fbd1019dfbd9a
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2023-11-12 20:55:48 -0500

    better progress bar

--- Project_Cooking/Assets/Scripts/UI/ProgressBar.cs
+++ Project_Cooking/Assets/Scripts/UI/ProgressBar.cs
@@ -7,16 +7,19 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     [SerializeField] private int max;
-    [SerializeField] private int current;
+    private int current = 0;
     [SerializeField] private Image mask;
+    [Header("Node sprite stuff")]
+    public List<Image> nodes;
+    [SerializeField] private Sprite unlitSprite;
+    [SerializeField] private Sprite litSprite;
 
-    [SerializeField] private Image fill;
-    [SerializeField] private Color fillColor;
+
 
 
     private void Start()
     {
-      //  fill.color = fillColor;
+
     }
     private void Update()
     {
@@ -25,12 +28,40 @@ public class ProgressBar : MonoBehaviour
     private void GetCurrentFill()
     {
         float fillAmt = (float)current / (float)max;
-       // mask.fillAmount = fillAmt;
+        mask.fillAmount = fillAmt;
+       
+    }
+   
+
+    // Call this method with the desired percentage and the total number of nodes
+    private void UpdateNodesVisuals(float percentage, int totalNodes)
+    {    
+        int litNodeCount = CalculateLitNodeCount(percentage);
+       
+        // Update the sprites based on the calculated lit node count
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            nodes[i].sprite = (i < litNodeCount) ? litSprite : unlitSprite;
+        }
+    }
+    private int CalculateLitNodeCount(float percentage)
+    {
+        if (percentage > 0.8f) return 4;
+        if (percentage > 0.6f) return 3;
+        if (percentage > 0.4f) return 2;
+        if (percentage > 0.2f) return 1;
+
+        return 0;
     }
 
     public void Increase(int amtToAdd)
     {
         current += amtToAdd;
+
+        if (current > max)
+            current = max;
+        float fillAmt = (float)current / (float)max;
+        UpdateNodesVisuals(fillAmt, nodes.Count);
     }
 
     public int GetCurrentBarAmt()
