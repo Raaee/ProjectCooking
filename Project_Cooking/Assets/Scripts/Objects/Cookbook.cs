c643b0cb67c646334bfc9861e871a13b036f6058
Raaee <varangurenv@gmail.com>
2023-11-19 20:05:36 -0500

    cookbook start

--- Project_Cooking/Assets/Scripts/Objects/Cookbook.cs
+++ Project_Cooking/Assets/Scripts/Objects/Cookbook.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cookbook : MonoBehaviour, IInteractable {
-    public void HighlightSprite() {
+
+    public List<WorkstationRecipe> allRecipes = new List<WorkstationRecipe>();
+    public List<WorkstationRecipe> recipesAlreadyCompleted = new List<WorkstationRecipe>();
+    public WorkstationRecipe levelRecipe;
+
+    private void Awake() {
+        PickRandomRecipe();
+    }
+
+    public void PickRandomRecipe() {
+       // var ran = new Random();
+        int ranNum;
+
+        do {
+           ranNum = Random.Range(0, allRecipes.Count);
+
+        } while (!recipesAlreadyCompleted.Contains(allRecipes[ranNum]));
+        
+        levelRecipe = allRecipes[ranNum];
+        recipesAlreadyCompleted.Add(levelRecipe);
     }
 
     public void Interact() {
         Debug.Log("Cookbook interact");
     }
-
+    
+    public void HighlightSprite() {
+    }
     public void NormalSprite() {
     }
 }
