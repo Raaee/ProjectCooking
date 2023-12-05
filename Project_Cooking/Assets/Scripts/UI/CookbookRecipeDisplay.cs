28517bf3d936ce3d89871c5393a4eb80b0548e10
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2023-12-05 08:13:33 -0500

    Update CookbookRecipeDisplay.cs

--- Project_Cooking/Assets/Scripts/UI/CookbookRecipeDisplay.cs
+++ Project_Cooking/Assets/Scripts/UI/CookbookRecipeDisplay.cs
@@ -8,24 +8,38 @@ public class CookbookRecipeDisplay : MonoBehaviour
     public Sprite chopSprite;
     public Sprite mixSprite;
     public Sprite stovenSprite;
+    private const int MAX_AMT_PANELS = 5;
     private void Start()
     {
+        DisplayAllRecipeSteps(4);
+    }
 
+    private void DisplayAllRecipeSteps(int playerUnlocked)//how many steps/nodes the player unlocked, should start at 1 not 0
+    {
+        
         //only show the correct amount of recipeStepUIDatas, change the i in the bottom iterator 
-        int amtOfRecipeSteps = recipeSO.recipeSteps.Count;
+        int maxAmtOfRecipeSteps = recipeSO.recipeSteps.Count;
+        if (playerUnlocked > maxAmtOfRecipeSteps)
+            playerUnlocked = maxAmtOfRecipeSteps;
 
-        //we are deleting the last (5 - amtOfRecipeSteps) panels
-        int panelsToHide = 5 - amtOfRecipeSteps;
-        for (int i = 4; i > panelsToHide; i--)
+
+        //we are hiding the last x panels
+        int panelsToHide = MAX_AMT_PANELS - playerUnlocked;
+        int tempPanelCOunt = 1;
+        for (int i = 4; i >= 0; i--)
         {
             recipeStepUIDataList[i].gameObject.SetActive(false);
+            tempPanelCOunt++;
+            if (tempPanelCOunt > panelsToHide)
+                break;
         }
 
-        for (int i = 0; i < amtOfRecipeSteps; i++)
+        for (int i = 0; i < maxAmtOfRecipeSteps; i++)
         {
             DisplayRecipeStep(recipeSO.recipeSteps[i], recipeStepUIDataList[i]);
         }
     }
+
     public void DisplayRecipeStep(RecipeStep recipeStep, RecipeStepUIData recipeStepUIData)
     {
         // assign the action image as the workstation
@@ -33,7 +47,7 @@ public class CookbookRecipeDisplay : MonoBehaviour
 
         //iterate through the recipe step ingredients and assign it to the images in recipestepuidatalist
         int amtOfRecipes = recipeStep.recipeIngredients.Count;
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)//at most 3 ingredients
         {
             if (i >= amtOfRecipes)
             {
@@ -43,9 +57,7 @@ public class CookbookRecipeDisplay : MonoBehaviour
             {
                 recipeStepUIData.recipeIngredientsImages[i].sprite = recipeStep.recipeIngredients[i].normalSprite;
             }
-
         }
-
     }
 
     private Sprite AssignSpriteFromRecipeAction(RecipeAction action)
@@ -60,7 +72,6 @@ public class CookbookRecipeDisplay : MonoBehaviour
                 return stovenSprite;
             default:
                 Debug.LogError("INVALID RECIPE ACTION");
-
                 break;
         }
         return null;
