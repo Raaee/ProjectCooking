03cc29f018de0f535134a2229f283ad2a3191943
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2023-12-31 11:41:18 -0500

    update some audio volumes

--- Project_Cooking/Assets/Scripts/Audio/MainMenuAudioControls.cs
+++ Project_Cooking/Assets/Scripts/Audio/MainMenuAudioControls.cs
@@ -1,12 +1,46 @@
+using System;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MainMenuAudioControls : MonoBehaviour
 {
     private int playerAudioAmt = 0;
     private const int MAX_AUDIO_AMOUNT = 100;
+    [Header("AUDIO REF")]
+    [SerializeField] private FMODUnity.EventReference passingNodeAudio;//1
+    [SerializeField] private FMODUnity.EventReference uiAccept;//2
+    [SerializeField] private FMODUnity.EventReference uiHover;//...
+    [SerializeField] private FMODUnity.EventReference bellRing;
+    [SerializeField] private FMODUnity.EventReference openBook;
+    [SerializeField] private FMODUnity.EventReference closeBook;
+    private FMODUnity.EventReference playedAudio;
+    [Header("AUDIO REF Ambi")]
+    [SerializeField] private FMODUnity.EventReference dungeonAmbi;
+    [SerializeField] private FMODUnity.EventReference dungeonSpecial;
+    private FMOD.Studio.EventInstance instance;
+    private FMOD.Studio.EventInstance instance2;
+    [Header("Slider REF")]
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+    [Header("Slider Text")]
+    [SerializeField] private TextMeshProUGUI musicText;
+    [SerializeField] private TextMeshProUGUI sfxText;
+
+
+    private FMOD.Studio.VCA musicVCA;
+    private FMOD.Studio.VCA sfxVCA;
+
     // Start is called before the first frame update
     void Start()
     {
+        instance = FMODUnity.RuntimeManager.CreateInstance(dungeonAmbi);
+        instance2 = FMODUnity.RuntimeManager.CreateInstance(dungeonSpecial);
+        PlayDungeon();
+        musicSlider.onValueChanged.AddListener(OnMusicVolChange);
+        sfxSlider.onValueChanged.AddListener(OnSfxVolChange);
+        musicVCA = FMODUnity.RuntimeManager.GetVCA("vca:/MusicVCA");
+        sfxVCA = FMODUnity.RuntimeManager.GetVCA("vca:/SFXVCA");
+        /*
         try
         {
 
@@ -21,8 +55,67 @@ public class MainMenuAudioControls : MonoBehaviour
         catch (System.Security.SecurityException)
         {
             Debug.Log("You do not have the required permissions");
-        }
+        }*/
     }
 
+   
+
+    //this is specifically for the Unity Button UI stiff
+    public void PlayAudio(int audioId)//too lazy to make all the play methods 
+    {
+        switch (audioId)
+        {
+            case 1:
+                playedAudio = passingNodeAudio;
+                break;
+            case 2:
+                playedAudio = uiAccept;
+                break;
+            case 3:
+                playedAudio = uiHover;
+                break;
+            case 4:
+                playedAudio = bellRing;
+                break;
+            case 5:
+                playedAudio = openBook;
+                break;
+            case 6:
+                playedAudio = closeBook;
+                break;
+            default:
+                Debug.Log("no audio id with this");
+                break;
+        }
+
+        FMODUnity.RuntimeManager.PlayOneShot(playedAudio, transform.position);
+    }
+
+    private void PlayDungeon()
+    {
+        instance.start();
+        instance2.start();
+    }
+
+    public void StopDungeon()
+    {
+        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        instance2.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void OnMusicVolChange(float newVolume)
+    {
+
+        musicText.text = ((int)(newVolume*100)).ToString();
+        musicVCA.setVolume(newVolume);
+    }
+
+    public void OnSfxVolChange(float newVolume)
+    {
+       
+        sfxText.text = ((int)(newVolume * 100)).ToString();
+        sfxVCA.setVolume(newVolume);
+       
+    }
 
 }
